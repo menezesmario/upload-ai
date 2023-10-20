8122d02203dc766b78b74d24a0aa28b64b25a100
Mário Menezes <mario082@gmail.com>
2023-10-19 22:35:30 -0300

    fix: custom button

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -7,10 +7,22 @@ import { UploadIcon } from '@radix-ui/react-icons';
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from 'react';
 import { getFFmpeg } from '@/lib/ffmpeg';
 import { fetchFile } from '@ffmpeg/util'
+import { api } from '@/lib/axios';
+
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+    converting: 'Convertendo...',
+    generating: 'Transcrevendo...',
+    uploading: 'Carregando...',
+    success: 'Sucesso!',
+}
 
 export function VideoInputForm() {
 
     const [videoFile, setVideoFile] = useState<File | null>(null)
+    const [status, setStatus] = useState<Status>('waiting')
+
     const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
     
@@ -73,10 +85,29 @@ export function VideoInputForm() {
         if(!videoFile) {
             return
         }
+
+        setStatus('converting')
         
         const audioFile = await convertVideoToAudio(videoFile)
 
-        console.log(audioFile, prompt) 
+        const data = new FormData()
+
+        data.append('file', audioFile)
+
+        setStatus('uploading')
+
+        const response = await api.post('/videos', data)
+
+        const videoId = response.data.video.id
+
+        setStatus('generating')
+
+
+        await api.post(`/videos/${videoId}/transcription`, {
+            prompt,
+        })
+
+        setStatus('success')
     }
 
     const previewURL = useMemo(() => {
@@ -104,10 +135,21 @@ export function VideoInputForm() {
         <Separator />
         <div className='space-y-2'>
          <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label> 
-         <Textarea ref={promptInputRef} id='transcription_prompt' className='h-20 leading-relaxed resize-none'
+         <Textarea disabled={status !== 'waiting'} ref={promptInputRef} id='transcription_prompt' className='h-20 leading-relaxed resize-none'
             placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)' />
         </div>    
-        <Button type='submit' className='w-full gap-2' >Carregar vídeo <UploadIcon className='w-4 h-4'/></Button>
+        <Button 
+            data-success={status === 'success'}
+            disabled={status !== 'waiting'} 
+            type='submit' 
+            className='w-full data-[success=true]:bg-emerald-400' >
+            {status === 'waiting' ? (
+                <>
+                    Carregar vídeo <UploadIcon className='w-4 h-4'/>
+                </>
+            ) : statusMessages[status]}
+            
+        </Button>
         
       </form>
     )
